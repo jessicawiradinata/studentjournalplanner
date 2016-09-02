efa9f4eec2518b7b9499a3618958a43529e5d160
Jessica <jessicawiradinata@gmail.com>
2016-09-02 20:48:37 +1000

    First Commit

---
+++ app/src/main/java/com/example/jessica/studentjournalplanner/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.jessica.studentjournalplanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupLoginButton();
+    }
+
+    private void setupLoginButton(){
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
+    }
+}
