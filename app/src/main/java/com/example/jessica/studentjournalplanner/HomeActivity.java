77278613de6ee0933583a8eb65f8d4a4fa9694c0
Jessica <jessicawiradinata@gmail.com>
2016-09-03 11:34:46 +1000

    Browse Button connected to Browse Page

--- app/src/main/java/com/example/jessica/studentjournalplanner/HomeActivity.java
+++ app/src/main/java/com/example/jessica/studentjournalplanner/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.jessica.studentjournalplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupBrowseButton();
+    }
+
+    private void setupBrowseButton(){
+        ImageButton browseButton = (ImageButton) findViewById(R.id.browseButton);
+        browseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(HomeActivity.this, BrowseActivity.class));
+            }
+        });
     }
 }
