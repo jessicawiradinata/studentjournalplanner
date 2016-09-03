1c8967943872fb4578d8890b299b4c63412d6a46
Jessica <jessicawiradinata@gmail.com>
2016-09-03 19:13:02 +1000

    Login Activity Revised

--- app/src/main/java/com/example/jessica/studentjournalplanner/BrowseActivity.java
+++ app/src/main/java/com/example/jessica/studentjournalplanner/BrowseActivity.java
@@ -1,7 +1,10 @@
 package com.example.jessica.studentjournalplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class BrowseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+        setupHomeButton();
+    }
+
+    private void setupHomeButton(){
+        ImageButton browseButton = (ImageButton) findViewById(R.id.homeButton);
+        browseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(BrowseActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
