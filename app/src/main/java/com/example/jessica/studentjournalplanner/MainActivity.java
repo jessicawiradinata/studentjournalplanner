dff4fd4a26777c0324cb1146fc478541924d72e4
Jessica <jessicawiradinata@gmail.com>
2016-09-27 12:53:34 +1000

    Login Connected to Firebase

--- app/src/main/java/com/example/jessica/studentjournalplanner/MainActivity.java
+++ app/src/main/java/com/example/jessica/studentjournalplanner/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.jessica.studentjournalplanner;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailField;
+    private EditText password;
+    private Button loginButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +32,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupLoginButton(){
-        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        emailField = (EditText) findViewById(R.id.emailField);
+        password = (EditText) findViewById(R.id.password);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, HomeActivity.class));
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn() {
+        String email = emailField.getText().toString();
+        String passwords = password.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(passwords)) {
+            Toast.makeText(MainActivity.this, "Please enter ID or password", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email, passwords).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent loginIntent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(loginIntent);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
     public void onButtonClick(View v){
